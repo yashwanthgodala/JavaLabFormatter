88e6edd9f0f7660542b4ecdc2b1c44f513637b81
yashwanthgodala <yashwanth.godala@gmail.com>
2026-09-19 19:36:01 +0530

    Basic imporvement of upload interface

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+const pdfFile = document.getElementById("pdfFile");
+const fileName = document.getElementById("fileName");
 const generateButton = document.getElementById("generateBtn");
+
+
+pdfFile.addEventListener("change", function () {
+
+    const file = pdfFile.files[0];
+
+    if (file) {
+        fileName.textContent = file.name;
+    } else {
+        fileName.textContent = "No file selected";
+    }
+
+});
+
 
 generateButton.addEventListener("click", function () {
 
-    const file = document.getElementById("pdfFile").files[0];
+    const file = pdfFile.files[0];
 
     if (!file) {
         alert("Please select a PDF first.");
         return;
     }
 
-    alert("PDF selected successfully!");
+    alert("PDF selected: " + file.name);
+
 });
